4da65fe33811d21b93e428c090fdd6466a9eed1e
Freddy Gutierrez <33851899+Freddy-Gutierrez@users.noreply.github.com>
2021-02-16 18:58:52 -0800

    made review page layout responsive

--- src/Components/review/feedback.jsx
+++ src/Components/review/feedback.jsx
@@ -3,37 +3,36 @@ import StarRatings from "react-star-ratings";
 
 const Feedback = (props) => {  
   return (
-    <div className="review-row">
+    <div className="review-flex">
       {props.reviews.map((review, index) => {
         return (
-          <div className="feedback-container" key={index}>
-            <div className="review-row">
-              <h3>{review.name}</h3>
-            </div>
+          <div style={{width:"100%"}}>
+            <h3 className="review-username">{review.name}</h3>
+            <div className="feedback-container" key={index}>
             <div className="two-col">
-              <p className="p-review">Rating</p>
-              <p className="p-review">Posted</p>
-            </div>
-            <div className="two-col">
-              <StarRatings
-                rating={review.rating}
-                starRatedColor="rgb(255,223,0)"
-                starEmptyColor="rgb(109, 122, 130)"
-                starDimension={"15px"}
-                starSpacing={"0px"}
-              />
-              <p style={{ marginLeft: "2em" }}>{review.date}</p>
-            </div>
-            <div className="review-row">
-              <div className="review-comment">{review.review}</div>
               <div>
-                <p
-                  className="helpful"
-                  onClick={(_id) => props.onClick(review._id)}
-                >{`Helpful(${review.helpful.length})`}</p>
+                <p className="p-review">Rating</p>
+                <StarRatings
+                  rating={review.rating}
+                  starRatedColor="rgb(255,223,0)"
+                  starEmptyColor="rgb(109, 122, 130)"
+                  starDimension={"15px"}
+                  starSpacing={"0px"}
+                />
+              </div>
+              <div>
+                <p className="p-review">Posted</p>
+                <p>{review.date}</p>
               </div>
             </div>
+            <div className="review-comment">{review.review}</div>
+            <p
+              className="helpful"
+              onClick={(_id) => props.onClick(review._id)}>
+                {`Helpful(${review.helpful.length})`}
+            </p>
           </div>
+        </div>
         );
       })}
     </div>
--- src/Components/review/reviews.jsx
+++ src/Components/review/reviews.jsx
@@ -82,51 +82,94 @@ class Reviews extends Component {
         <h1>Be the first to leave a review!</h1>
         <Link to={`/feedback-form/${productId}`} className="review-button">Write a review</Link>
       </div> :
-      <div className="review-container">
+      <div className="container grid grid-cols-3 review">
         <div className="stars-container">
-          <h3>{`Overall rating ${productReview.avgRating}`}</h3>
-          <div className="break"></div>
-          <ReviewStars 
-            totalReviews={productReview.reviews.length}
-            fiveStars={fiveStarReviews.length}
-            fourStars={fourStarReviews.length}
-            threeStars={threeStarReviews.length}
-            twoStars={twoStarReviews.length}
-            oneStars={oneStarReviews.length}
-            onClick={this.showReviews}
-          />
-        </div>
-        <div className="review-content">
-          <div className="review-row">
-            <h1>All Reviews</h1>
-            <Link to={`/feedback-form/${productReview.productId}`} className="review-button">Write a review</Link>
-          </div>
-          <div className="review-row">
-            <Select
-              title="Sort By"
-              options={["Recent", "Helpful"]}
-              onClick={this.sortReviews}
-            />
-            <Select
-              title="Show"
-              options={[
-                "All Ratings",
-                "5 Stars",
-                "4 Stars",
-                "3 Stars",
-                "2 Stars",
-                "1 Star",
-              ]}
+            <h2>{`Overall rating ${productReview.avgRating}`}</h2>            
+            <ReviewStars 
+              totalReviews={productReview.reviews.length}
+              fiveStars={fiveStarReviews.length}
+              fourStars={fourStarReviews.length}
+              threeStars={threeStarReviews.length}
+              twoStars={twoStarReviews.length}
+              oneStars={oneStarReviews.length}
               onClick={this.showReviews}
             />
           </div>
-          <Feedback
-            reviews={displayedReviews}
-            onClick={this.updateHelpful}
-          />
-        </div>
-        <ToastContainer position="bottom-center"/>
-      </div>      
+          <div className="review-content">
+            <div className="review-flex">
+              <h1>All Reviews</h1>
+              <Link to={`/feedback-form/${productReview.productId}`} className="review-button">Write a review</Link>
+            </div>
+            <div className="review-flex">
+              <Select
+                title="Sort By"
+                options={["Recent", "Helpful"]}
+                onClick={this.sortReviews}
+              />
+              <Select
+                title="Show"
+                options={[
+                  "All Ratings",
+                  "5 Stars",
+                  "4 Stars",
+                  "3 Stars",
+                  "2 Stars",
+                  "1 Star",
+                ]}
+                onClick={this.showReviews}
+              />
+            </div>
+            <Feedback              
+              reviews={displayedReviews}
+              onClick={this.updateHelpful}
+            />
+          </div>
+      </div>
+      // <div className="review-container">
+        // <div className="stars-container">
+        //   <h3>{`Overall rating ${productReview.avgRating}`}</h3>
+        //   <div className="break"></div>
+        //   <ReviewStars 
+        //     totalReviews={productReview.reviews.length}
+        //     fiveStars={fiveStarReviews.length}
+        //     fourStars={fourStarReviews.length}
+        //     threeStars={threeStarReviews.length}
+        //     twoStars={twoStarReviews.length}
+        //     oneStars={oneStarReviews.length}
+        //     onClick={this.showReviews}
+        //   />
+        // </div>
+        // <div className="review-content">
+        //   <div className="review-flex">
+        //     <h1>All Reviews</h1>
+        //     <Link to={`/feedback-form/${productReview.productId}`} className="review-button">Write a review</Link>
+        //   </div>
+        //   <div className="review-flex">
+        //     <Select
+        //       title="Sort By"
+        //       options={["Recent", "Helpful"]}
+        //       onClick={this.sortReviews}
+        //     />
+        //     <Select
+        //       title="Show"
+        //       options={[
+        //         "All Ratings",
+        //         "5 Stars",
+        //         "4 Stars",
+        //         "3 Stars",
+        //         "2 Stars",
+        //         "1 Star",
+        //       ]}
+        //       onClick={this.showReviews}
+        //     />
+        //   </div>
+        //   <Feedback
+        //     reviews={displayedReviews}
+        //     onClick={this.updateHelpful}
+        //   />
+        // </div>
+      //   <ToastContainer position="bottom-center"/>
+      // </div>      
     );
   }
 }
